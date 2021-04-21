e06f8817b477136b4ca8ad8b43de10f412145ce4
Yauhen Kruchkou <kruchkovevg21@gmail.com>
2021-04-21 21:11:50 +0300

    server/db/add auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,21 @@
 import {Module} from "@nestjs/common";
 import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from "./file/files.module";
 import { TrackModule } from "./track/track.module";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path'
+
+const user = 'chad';
+const password = 'fuckingpassword1W';
 
 @Module({
     imports:[
         TrackModule,
-        MongooseModule.forRoot('mongodb://178.62.77.223:27017/api')
+        MongooseModule.forRoot(`mongodb://${user}:${password}@178.62.77.223:27017`, {
+            useNewUrlParser: true
+        }),
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+        FileModule
     ],
     controllers:[],
     providers: []
